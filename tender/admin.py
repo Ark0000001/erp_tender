5deaf8fc13fa879495c26841fea69c17081fc54c
hp <yotarona@gmail.com>
2022-10-03 13:08:56 +0500

    обновление2

--- tender/admin.py
+++ tender/admin.py
@@ -5,15 +5,19 @@ from import_export import resources,fields
 from .models import *
 from import_export.admin import ImportExportModelAdmin
 from import_export.widgets import ForeignKeyWidget
+
+from import_export.tmp_storages import CacheStorage
+
+
+
+
+
 admin.site.register(Type_tender)
 
 
 class RegResource(resources.ModelResource):
-
     class Meta:
         model = Region
-
-
 class RegAdmin(ImportExportModelAdmin):
     list_display = ('name',)
     list_display_links = ('name', )
@@ -21,40 +25,31 @@ class RegAdmin(ImportExportModelAdmin):
     resource_class=RegResource
 admin.site.register(Region,RegAdmin)
 
-
-
 class CityResource(resources.ModelResource):
-    region = fields.Field(
-        column_name='region',
-        attribute='region',
-        widget=ForeignKeyWidget(Region,'name'))
-
     class Meta:
         model = City
-        fields = 'region'
-
-
 class CityAdmin(ImportExportModelAdmin):
     list_display = ('name', 'region')
     list_display_links = ('name', 'region')
-    search_fields = ('name', 'region')
+    search_fields = ('name',)
     resource_class=CityResource
 admin.site.register(City,CityAdmin)
 
-
-class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'inn')
-    list_display_links = ('name', 'inn')
-    search_fields = ('name','inn')
+class CompanyResource(resources.ModelResource):
+    class Meta:
+        model = Company
+class CompanyAdmin(ImportExportModelAdmin):
+    list_display = ('name', )
+    list_display_links = ('name', )
+    search_fields = ('name',)
+    resource_class = CompanyResource
 admin.site.register(Company,CompanyAdmin)
 
-
-class DealerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'company')
-    list_display_links = ('name',)
+class OrganizationsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'company', 'inn')
+    list_display_links = ('name','inn')
     search_fields = ('name',)
-admin.site.register(Dealer,DealerAdmin)
-
+admin.site.register(Organizations,OrganizationsAdmin)
 
 class ClientAdmin(admin.ModelAdmin):
     list_display = ('name', 'inn')
@@ -62,13 +57,26 @@ class ClientAdmin(admin.ModelAdmin):
     search_fields = ('name','inn')
 admin.site.register(Client,ClientAdmin)
 
-
-class ProductAdmin(admin.ModelAdmin):
+class GroupResource(resources.ModelResource):
+    class Meta:
+        model = Group_prod
+class GroupAdmin(ImportExportModelAdmin):
     list_display = ('name',)
     list_display_links = ('name',)
     search_fields = ('name',)
-admin.site.register(Product,ProductAdmin)
+    resource_class = GroupResource
+admin.site.register(Group_prod,GroupAdmin)
 
+class ProductResource(resources.ModelResource):
+    class Meta:
+        model = Product
+class ProductAdmin(ImportExportModelAdmin):
+    tmp_storage_class = CacheStorage
+    list_display = ('group_prod','name','my_id','price','ss', 'status')
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    resource_class =ProductResource
+admin.site.register(Product,ProductAdmin)
 
 class FilialAdmin(admin.ModelAdmin):
     list_display = ('name',)
@@ -76,23 +84,25 @@ class FilialAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 admin.site.register(Filial,FilialAdmin)
 
-
 class StafferAdmin(admin.ModelAdmin):
     list_display = ('name',)
     list_display_links = ('name',)
     search_fields = ('name',)
 admin.site.register(Staffer,StafferAdmin)
 
-
+from django.forms import CheckboxSelectMultiple
 class TabAdmin(admin.ModelAdmin):
-    list_display = ('number_tender','protection','data1','data2','price1','type_tender','city','client','product','dealer','filial','win','price2')
+    formfield_overrides = {
+        models.ManyToManyField: {'group_prod': CheckboxSelectMultiple},
+    }
+    list_display = ('number_tender','url_tender','number_scheta','number_zakaza','protection','data1','data2','price1','type_tender','city','client','group_prod','info','filial','win','price2')
     list_display_links = ('number_tender','type_tender','client')
     search_fields = ('client__name',)
 admin.site.register(Tab,TabAdmin)
 
 
 class ProtectionAdmin(admin.ModelAdmin):
-    list_display = ('city','client','product','dealer','data2')
-    list_display_links = ('city','client','product','dealer','data2')
-    search_fields = ('client__name','dealer_name')
+    list_display = ('city','client','product_info','company','data2')
+    list_display_links = ('city','client','product_info','company','data2')
+    search_fields = ('client__name','company_name')
 admin.site.register(Protection,ProtectionAdmin)
--- tender/models.py
+++ tender/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
 # Create your models here.
-
-
 class Type_tender(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, verbose_name='Вид тендера')
     nomer=models.IntegerField(null=True,verbose_name='Порядковый номер')
 
     def __str__(self):
@@ -16,8 +14,8 @@ class Type_tender(models.Model):
         verbose_name='Тип конкурса'
 
 class Region(models.Model):
-    name = models.CharField(max_length=200)
-    my_id=models.IntegerField(unique=True)
+    name = models.CharField(max_length=200, verbose_name='Наименование')
+    my_id=models.IntegerField(unique=True, default='1')
 
     def __str__(self):
         return str(self.name)
@@ -28,42 +26,37 @@ class Region(models.Model):
         verbose_name='Регион'
 
 class City(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, verbose_name='Наименование')
     region=models.ForeignKey(Region,on_delete=models.SET_NULL, null=True,verbose_name='Регион')
-
     def __str__(self):
         return str(self.name)
-
     class Meta:
         ordering = ['region','name']
         verbose_name_plural='Города'
         verbose_name='Город'
 
 class Company(models.Model):
-    name = models.CharField(max_length=200)
-    inn=models.CharField(max_length=50, verbose_name='ИНН')
-
-    def __str__(self):
-        return f'{(self.name)}, ИНН: {self.inn}'
-
-    class Meta:
-        ordering = ['name']
-        verbose_name_plural='Юридические лица'
-        verbose_name='Юридическое лицо'
-
-class Dealer(models.Model):
-    name = models.CharField(max_length=200)
-    company=models.ForeignKey(Company,on_delete=models.SET_NULL, null=True,verbose_name='Юридическое лицо')
-
-
+    name = models.CharField(max_length=200, verbose_name='Контрагент')
 
     def __str__(self):
         return str(self.name)
-
     class Meta:
         ordering = ['name']
         verbose_name_plural='Контрагенты'
         verbose_name='Контрагент'
+
+class Organizations(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Юридические лица')
+    inn = models.CharField(max_length=50, verbose_name='ИНН')
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True, verbose_name='Контрагент')
+
+    def __str__(self):
+        return f'{(self.name)}, Контрагент: {self.company}'
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'Юридические лица'
+        verbose_name = 'Юридическое лицо'
 
 class Client(models.Model):
     name = models.CharField(max_length=200)
@@ -78,9 +71,24 @@ class Client(models.Model):
         verbose_name_plural='Заказчики'
         verbose_name='Заказчик'
 
-class Product(models.Model):
-    name = models.CharField(max_length=200)
+class Group_prod(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Товарная группа')
+    nomer=models.IntegerField(null=True,verbose_name='Порядковый номер')
+    def __str__(self):
+        return str(self.name)
+    class Meta:
+        ordering = ['nomer']
+        verbose_name_plural='Товарные группы'
+        verbose_name='Товарная группа'
 
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Наименование')
+    my_id = models.CharField(max_length=50,verbose_name='Артикул', default='0')
+    price = models.CharField(max_length=10,blank=True, default='0.00', verbose_name='Цена')
+    status = models.CharField(max_length=2, verbose_name='Статус', null=True)
+    ss = models.CharField(max_length=10,blank=True, default='0.00', verbose_name='СС с НДС УЗМК')
+    price2 = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True, verbose_name='Цена контракта')
+    group_prod = models.ForeignKey(Group_prod, on_delete=models.SET_NULL, null=True, verbose_name='Товарная группа')
     def __str__(self):
         return str(self.name)
 
@@ -90,7 +98,7 @@ class Product(models.Model):
         verbose_name='Продукт'
 
 class Filial(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200,verbose_name='Юр. лицо филиала (Техноимпорт, Промет, Инвалиды и тп')
 
     def __str__(self):
         return str(self.name)
@@ -101,43 +109,48 @@ class Filial(models.Model):
         verbose_name='Филиал'
 
 class Staffer(models.Model):
-    name = models.CharField(max_length=200)
-
+    name = models.CharField(max_length=200,verbose_name='Фамилия сотрудника')
     def __str__(self):
         return str(self.name)
-
     class Meta:
         ordering = ['name']
         verbose_name_plural = 'Сотрудники'
         verbose_name = 'Сотрудник'
 
 class Tab(models.Model):
-    YES='Да'
-    NO='Нет'
-    CHOICES = [(YES, 'Да'), (NO, 'Нет'),]
-    protection = models.CharField(max_length=3, choices=CHOICES, default=YES,verbose_name='Защита' )
-
+    # YES='Да'
+    # NO='Нет'
+    # CHOICES = [(YES, 'Да'), (NO, 'Нет'),]
+    # protection = models.CharField(max_length=3, choices=CHOICES, default=YES,verbose_name='Защита')
+    protection = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True, verbose_name='Защита')
+    protection_info = models.CharField(max_length=200, blank=True, verbose_name='Примечание по защите')
     F = 'Филиал'
     D = 'Дилер'
     N = 'Другой'
-
-    CHOI = [(F, 'Филиал'), (D, 'Дилер'), (N, 'Другой'), ]
+    Net = 'Нет'
+    CHOI = [(F, 'Филиал'), (D, 'Дилер'), (N, 'Другой'), (Net, 'Нет'),]
     win = models.CharField(max_length=6, choices=CHOI, default=F,verbose_name='Победитель')
 
+    number_tender = models.CharField(max_length=50,blank=True, verbose_name='Номер тендера или ссылка')
+    number_zakaza = models.CharField(max_length=50, blank=True, verbose_name='Номер заказа')
+    number_scheta = models.CharField(max_length=50, blank=True, verbose_name='Номер счета')
+    schet_info = models.CharField(max_length=200, blank=True, verbose_name='Примечание к счету (R%, затраты и тп)')
+    url_tender = models.CharField(max_length=50, blank=True, verbose_name='Ссылка на папку с тендером')
 
-    number_tender = models.CharField(max_length=50, verbose_name='Номер тендера')
+
     type_tender=models.ForeignKey(Type_tender,on_delete=models.SET_NULL, null=True,verbose_name='Тип тендера')
     city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, verbose_name='Город')
-    client = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True, verbose_name='Заказчик')
-    price1=models.DecimalField(max_digits=20, decimal_places=2, verbose_name='Начальная цена')
+    client = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True,blank=True, verbose_name='Заказчик')
+    price1=models.DecimalField(max_digits=20, decimal_places=2, null=True,blank=True, verbose_name='Начальная цена')
 
-    data1=models.DateTimeField(null=True,verbose_name='Дата начала')
-    data2 = models.DateTimeField(null=True, verbose_name='Дата конца')
-    product=models.ForeignKey(Product,on_delete=models.SET_NULL, null=True,verbose_name='Продукт')
-    dealer=models.ForeignKey(Dealer,on_delete=models.SET_NULL, null=True,verbose_name='Дилер')
+    data1=models.DateTimeField(null=True,blank=True,verbose_name='Дата начала')
+    data2 = models.DateTimeField(null=True,blank=True, verbose_name='Дата конца')
+    group_prod=models.ForeignKey(Group_prod,on_delete=models.SET_NULL, null=True,verbose_name='Товарная группа 1')
+
+    info=models.CharField(max_length=200,blank=True, verbose_name='Примечание к товарным группам')
     filial=models.ForeignKey(Filial,on_delete=models.SET_NULL, null=True,verbose_name='Филиал')
     price2 = models.DecimalField(max_digits=20, decimal_places=2,blank=True, null=True,verbose_name='Цена контракта')
-
+    staffer = models.ForeignKey(Staffer, on_delete=models.SET_NULL, null=True, verbose_name='отв. Сотрудник')
     def __str__(self):
         return f'{self.number_tender} Заказчик: {self.client}'
 
@@ -146,17 +159,16 @@ class Tab(models.Model):
         verbose_name_plural = 'Данные таблицы тендеров'
         verbose_name = 'Данные тендеров'
 
-
 class Protection(models.Model):
     data1 = models.DateTimeField(null=True, verbose_name='Дата внесения')
     city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, verbose_name='Город')
     client = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True, verbose_name='Заказчик')
-    data2 = models.DateTimeField(null=True, verbose_name='Должен состояться до: ')
-    product=models.ForeignKey(Product,on_delete=models.SET_NULL, null=True,verbose_name='Продукт')
-    dealer=models.ForeignKey(Dealer,on_delete=models.SET_NULL, null=True,verbose_name='Дилер')
+    data2 = models.DateTimeField(null=True, blank=True, verbose_name='Должен состояться до: ')
+    product_info = models.TextField(max_length=500, blank=True, verbose_name='Примечание по ТМЦ')
+    company=models.ForeignKey(Company,on_delete=models.SET_NULL, null=True,verbose_name='Дилер')
 
     def __str__(self):
-        return str(self.dealer)
+        return str(self.company)
 
     class Meta:
         ordering = ['data1']
